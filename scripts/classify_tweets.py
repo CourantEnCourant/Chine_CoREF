390b5db0d83667f88e5e58fb1b0fa96bfc6440cd
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2024-06-21 15:52:38 +0200

    fix: debug

--- scripts/classify_tweets.py
+++ scripts/classify_tweets.py
@@ -9,7 +9,7 @@ id2label = {7: '涉案', 10: '都市', 12: '革命', 4: '农村', 0: '传奇', 2
             1: '传记', 11: '青少', 3: '军旅', 6: '武打', 9: '科幻', 8: '神话', 5: '宫廷'}
 
 
-def main(input_txt, output_csv) -> None:
+def predict_agriculture(input_txt, output_csv) -> None:
     """Classify texts"""
     # Load text
     with open(input_txt, 'r', encoding='utf-8') as f:
@@ -17,7 +17,7 @@ def main(input_txt, output_csv) -> None:
     print(f'{len(texts)} texts to classify.')
 
     # Check device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     print(f'Using {device}.')
 
     # Load model
@@ -30,17 +30,18 @@ def main(input_txt, output_csv) -> None:
     idx = 1
     for text in tqdm(texts):
         inputs = tokenizer(text, truncation=True, max_length=512, return_tensors='pt')
+        inputs = inputs.to(device)
         outputs = model(**inputs)
         label_id = torch.argmax(outputs.logits, dim=1).to('cpu').numpy()[0]
-        label = [id2label[label_id]]
+        label = id2label[label_id]
         if label == '农村':
-            text_classified = (idx, text, label)
+            text_classified = (str(idx), str(text), str(label))
             texts_classified.append(text_classified)
             idx += 1
 
     # Output to csv
     with open(output_csv, 'w', encoding='utf-8') as f:
-        headers = 'idx, text, label'
+        headers = 'idx,text,label'
         f.write(headers + '\n')
         for text_classified in texts_classified:
             text = ','.join(text_classified)
@@ -48,5 +49,5 @@ def main(input_txt, output_csv) -> None:
 
 
 if __name__ == '__main__':
-    main(input_txt='../data/raw/tweets.txt',
-         output_csv='../data/raw/tweets_agriculture.csv')
+    predict_agriculture(input_txt='../data/raw/tweets.txt',
+                        output_csv='../data/raw/tweets_agriculture.csv')
